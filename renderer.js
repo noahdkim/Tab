0f0c75433bae5df3349685bc8b0a5add76cbc9e2
Noah Kim <nk8na@virginia.edu>
2019-01-29 22:18:19 -0500

    Firebase integration

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 /* Initialize Firebase */
+var firebase = require("firebase");
 var config = {
   apiKey: "AIzaSyBjkYFQB-DX0O9RKf_NFktYiHdHZkSDDX4",
   authDomain: "tab-electron.firebaseapp.com",
@@ -14,4 +15,30 @@ function getLoginInfo(){
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
   console.log("email: " + email);
+}
+
+
+function signIn() {
+  console.log("signIn function");
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  /* Sign-in current user */
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    /* Output errors to console */
+    console.log("Error code: " + error.code + "\nError message: " + error.message);
+  });
+
+}
+
+function signUp() {
+  console.log("signUp function");
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  /* Create new user */
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    /* Output errors to console */
+    console.log("Error code: " + error.code + "\nError message: " + error.message);
+  });
 }
