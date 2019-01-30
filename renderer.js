fddd1bbc6d08321950a3d80c2c9c5464f1f2fdbf
johnjp15 <johnjp15@gmail.com>
2019-01-29 22:56:25 -0500

    Cleaned up package.json dependencies. Made signIn and signUp buttons function with Firebase auth.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,4 @@
 /* Initialize Firebase */
-var firebase = require("firebase");
 var config = {
   apiKey: "AIzaSyBjkYFQB-DX0O9RKf_NFktYiHdHZkSDDX4",
   authDomain: "tab-electron.firebaseapp.com",
@@ -17,28 +16,29 @@ function getLoginInfo(){
   console.log("email: " + email);
 }
 
+var signInBtn = document.getElementById('signInBtn');
+var signUpBtn = document.getElementById('signUpBtn');
 
-function signIn() {
-  console.log("signIn function");
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+signInBtn.addEventListener('click', function()   {
+    console.log("signIn function");
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-  /* Sign-in current user */
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    /* Output errors to console */
-    console.log("Error code: " + error.code + "\nError message: " + error.message);
-  });
+    /* Sign-in current user */
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      /* Output errors to console */
+      console.log("Error code: " + error.code + "\nError message: " + error.message);
+    });
+});
 
-}
+signUpBtn.addEventListener('click', function()   {
+    console.log("signUp function");
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-function signUp() {
-  console.log("signUp function");
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-
-  /* Create new user */
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    /* Output errors to console */
-    console.log("Error code: " + error.code + "\nError message: " + error.message);
-  });
-}
+    /* Create new user */
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      /* Output errors to console */
+      console.log("Error code: " + error.code + "\nError message: " + error.message);
+    });
+});
