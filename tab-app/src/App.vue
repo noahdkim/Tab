7a1070ccec936a25be9fdaf771df003d1cd6487b
johnjp15 <johnjp15@gmail.com>
2019-02-04 23:33:56 -0500

    Moved tab-app-electron relevant src files to tab-app

--- tab-app/src/App.vue
+++ tab-app/src/App.vue
@@ -1,29 +1,90 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app>
+    <v-navigation-drawer v-model="sidebar" app>
+        <v-list>
+            <v-list-tile v-for="item in menuItems" :key="item.title" :to="item.path">
+                <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile v-if="isAuthenticated" @click="userSignOut">
+                <v-list-tile-action>
+                    <v-icon>exit_to_app</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>Sign Out</v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app>
+        <span class="hidden-sm-and-up">
+            <v-toolbar-side-icon @click="sidebar = !sidebar">
+            </v-toolbar-side-icon>
+        </span>
+        <v-toolbar-title>
+            <router-link :to="isAuthenticated ? '/home' : '/'" tag="span" style="cursor: pointer">
+                {{ appTitle }}
+            </router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+            <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+                <v-icon left dark>{{ item.icon }}</v-icon>
+                {{ item.title }}
+            </v-btn>
+            <v-btn flat v-if="isAuthenticated" @click="userSignOut">
+                <v-icon left>exit_to_app</v-icon>
+                Sign Out
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+        <router-view></router-view>
+    </v-content>
+
+</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+    data() {
+        return {
+            // appTitle: 'Awesome App',
+            sidebar: false,
+        }
+    },
+    computed: {
+        appTitle() {
+            return this.$store.state.appTitle
+        },
+        isAuthenticated() {
+            return this.$store.getters.isAuthenticated
+        },
+        menuItems() {
+            if (this.isAuthenticated) {
+                return [{
+                    title: 'Home',
+                    path: '/home',
+                    icon: 'home'
+                }]
+            } else {
+                return [{
+                        title: 'About Us',
+                        path: '/aboutus',
+                        icon: 'face'
+                    }
+
+                ]
+            }
+        }
+    },
+    methods: {
+        userSignOut() {
+            this.$store.dispatch('userSignOut')
+        }
+    },
+
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ tab-app/src/components/Home/List.vue
@@ -0,0 +1,10 @@
+<template>
+<v-layout column>
+    <v-flex xs12 class="text-xs-center" mt-5>
+      <h1>Home page</h1>
+    </v-flex>
+    <v-flex xs12 class="text-xs-center" mt-3>
+      <p>This is a user's home page</p>
+    </v-flex>
+</v-layout>
+</template>
