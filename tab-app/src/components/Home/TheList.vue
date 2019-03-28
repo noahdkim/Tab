753172300816ea2a8d23d1134f11e1e102ea4fcd
johnjp15 <johnjp15@gmail.com>
2019-03-28 16:42:00 -0400

    draggable successful test-of-concept

--- tab-app/src/components/Home/TheList.vue
+++ tab-app/src/components/Home/TheList.vue
@@ -5,24 +5,30 @@
                 <div class="font-weight-bold">{{header}}</div>
             </v-layout>
         </v-layout>
-        <list-row v-for="(item, index) in selectedListItems"
-                    :key="index"
-                    :item="item"
-                    :headers="selectedListHeaders"
-                    :ref="item.id"
-                    @clicked="modifyActive"
-                    >
-        </list-row>
+        <draggable>
+            <list-row v-for="(item, index) in selectedListItems"
+                        :key="index"
+                        :item="item"
+                        :headers="selectedListHeaders"
+                        :ref="item.id"
+                        @clicked="modifyActive"
+                        >
+            </list-row>
+        </draggable>
         <v-btn @click.native="saveList">Save</v-btn>
     </v-container>
 
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListRow from './List/ListRow'
 import { mapGetters } from 'vuex'
 export default {
-    components: {ListRow},
+    components: {
+        draggable,
+        ListRow,
+    },
     data: () => ({
 
     }),
