13ef0f868d7f90c699a054a0fb3667d812313b13
John <john@ubuntu.ubuntu-domain>
2019-03-28 21:28:34 -0400

    change list-row to li for now.

--- tab-app/src/components/Home/TheList.vue
+++ tab-app/src/components/Home/TheList.vue
@@ -5,15 +5,35 @@
                 <div class="font-weight-bold">{{header}}</div>
             </v-layout>
         </v-layout>
-        <draggable>
-            <list-row v-for="(item, index) in selectedListItems"
+        <draggable 
+                    tag="ul"    :list="selectedListItems"
+                    class="list-group"
+                    handle=".handle">
+
+                    <!-- temporarily commented out -->
+            <!-- <list-row v-for="(item, index) in selectedListItems"
                         :key="index"
                         :item="item"
                         :headers="selectedListHeaders"
                         :ref="item.id"
                         @clicked="modifyActive"
                         >
-            </list-row>
+            </list-row> -->
+
+<!-- https://github.com/SortableJS/Vue.Draggable/blob/master/example/components/handle.vue#L11 -->
+            <li
+              class="list-group-item"
+              v-for="(item, index) in selectedListItems"
+              :key="index"
+            >
+              <i class="fa fa-align-justify handle"></i>
+
+              <span class="text">{{ item.id }} </span>
+
+              <input type="text" class="form-control" v-model="item.item" />
+
+              <i class="fa fa-times close"></i>
+            </li>
         </draggable>
         <v-btn @click.native="saveList">Save</v-btn>
     </v-container>
@@ -53,5 +73,37 @@ export default {
             }
 
     }
-  }
+  };
 </script>
+<style scoped>
+.button {
+  margin-top: 35px;
+}
+.handle {
+  float: left;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+.close {
+  float: right;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+input {
+  display: inline-block;
+  width: 50%;
+}
+.text {
+  margin: 20px;
+}
+
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
