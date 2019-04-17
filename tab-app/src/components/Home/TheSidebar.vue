12fd05049025b26c5d95038264800e81f7482b1d
Noah Kim <nk8na@virginia.edu>
2019-04-17 18:55:28 -0400

    add button that doesn't do anything yet

--- tab-app/src/components/Home/TheSidebar.vue
+++ tab-app/src/components/Home/TheSidebar.vue
@@ -13,6 +13,19 @@
          <v-list-tile-title>{{ personalList.name }}</v-list-tile-title>
        </v-list-tile-content>
      </v-list-tile>
+
+     <v-list-tile
+     @click=""
+     >
+       <v-list-tile-action>
+           <v-icon>add</v-icon>
+       </v-list-tile-action>
+       <v-list-tile-content>
+         <v-list-tile-title>Add List</v-list-tile-title>
+       </v-list-tile-content>
+     </v-list-tile>
+
+
    </v-list>
 </v-navigation-drawer>
 </template>
