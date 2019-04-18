d584ea7b884e5e812cb75206a18a698864f7f8dd
johnjp15 <johnjp15@gmail.com>
2019-04-18 14:38:36 -0400

    Transition changes. Trash can icon still not fading out correctly. TODO

--- tab-app/src/components/Home/List/ListRow.vue
+++ tab-app/src/components/Home/List/ListRow.vue
@@ -1,28 +1,38 @@
 <template>
   <v-layout row
-            :id="item.item_meta.id"
-            @click="makeActive"
-            @mouseover="mouseOver()"
-            @mouseleave="mouseLeave()"
-            >
-    <!-- <span class="fa fa-align-justify handle">&#9776;</span> -->
-    <span id="handle_id" class="fa fa-align-justify handle"
-            v-show=showHandle>::</span>
-          <!-- v-show=showHandle -->
-    <span class="fa fa-align-justify handle" style="color: rgba(255,255,255,0);"
-            v-show=!showHandle>::</span>
-    <v-layout col v-for="header in headers" :key="header.id">
-      <list-cell :item = "item"
-                      :header = "header"
-                      ref="{{item.item_meta.id}}-{{header.text}}"
-                      single-line>
-      </list-cell>
+  :id="item.item_meta.id"
+  @click="makeActive"
+  @mouseover="mouseOver()"
+  @mouseleave="mouseLeave()"
+  >
+  <!-- <span class="fa fa-align-justify handle">&#9776;</span> -->
+  <transition name="fade" mode="out-in">
+    <div key="3" v-if="showHandle">
+        <span id="handle_id" class="fa fa-align-justify handle">::</span>
+    </div>
+    <div key="1" v-if="!showHandle">
+        <span class="fa fa-align-justify handle" style="color: rgba(255,0,255,0.5);">::</span>
+    </div>
+</transition>
+<v-layout col v-for="header in headers" :key="header.id">
+    <list-cell :item = "item"
+    :header = "header"
+    ref="{{item.item_meta.id}}-{{header.text}}"
+    single-line>
+</list-cell>
 
 
-  </v-layout>
-  <v-btn flat @click="deleteItem" v-show="showHandle">
-      <v-icon>delete</v-icon>
-  </v-btn>
+</v-layout>
+<v-btn flat @click="deleteItem" >
+    <transition name="fade" mode="out-in">
+    <div key="3" v-if="showHandle">
+        <v-icon style="opacity: 0.3">delete</v-icon>
+    </div>
+    <div key="1" v-if="!showHandle">
+        <v-icon style="color: #0f0; opacity: 0.3">delete</v-icon>
+    </div>
+</transition>
+</v-btn>
 
 
 </v-layout>
@@ -42,37 +52,37 @@
         checkbox: true,
 
         showHandle: false,
-      }
-    },
-    methods: {
-        deleteItem (event) {
-            this.$store.dispatch('deleteItem', this.item);
-        },
-        makeActive (event) {
+    }
+},
+methods: {
+  deleteItem (event) {
+    this.$store.dispatch('deleteItem', this.item);
+},
+makeActive (event) {
               // When the row is clicked, dispatch changeActiveItem to the store passing
               // the current ID.
               // the changeActiveItem method searches for the ID and modifies the active attribute
               // to true
               this.showHandle = false;
               this.$store.dispatch('changeActiveItem', this.item);
-        },
-        mouseOver(event)    {
-            this.showHandle = true;
-            var vm = this;
+          },
+          mouseOver(event)    {
+              this.showHandle = true;
+              var vm = this;
 
-            EventBus.$on('the-list-drag-event', drag => {
+              EventBus.$on('the-list-drag-event', drag => {
                 // is this necessary? it seems to slow down code a good amount
                 if(drag)    {
-                    this.showHandle = false;
-                }
-            });
-        },
-        mouseLeave(event)   {
-            this.showHandle = false;
-        }
+                  this.showHandle = false;
+              }
+          });
+          },
+          mouseLeave(event)   {
+              this.showHandle = false;
+          }
 
-}
-}
+      }
+  }
 
 
 
@@ -80,39 +90,56 @@
 
 <style scoped>
 .button {
-  /*margin-top: 35px;*/
+    /*margin-top: 35px;*/
 }
 .handle {
-  padding: 5px;
-  margin-left: 10px;
-  margin-right: 10px;
-  /*border: solid #000 1px;*/
-  cursor: grab;
-  font-size: 200%;
-  /*margin-bottom: 30px;*/
+    padding: 5px;
+    margin-left: 10px;
+    margin-right: 10px;
+    /*border: solid #000 1px;*/
+    cursor: grab;
+    font-size: 200%;
+    /*margin-bottom: 30px;*/
 
-  color: rgba(0, 0, 0, 0.3);
+    color: rgba(0, 0, 0, 0.3);
 }
 .close {
-  float: right;
-  /*padding-top: 8px;*/
-  /*padding-bottom: 8px;*/
+    float: right;
+    /*padding-top: 8px;*/
+    /*padding-bottom: 8px;*/
 }
 input {
-  display: inline-block;
-  /*width: 50%;*/
+    display: inline-block;
+    /*width: 50%;*/
 }
 .text {
-  margin: 0px;
+    margin: 0px;
 }
 
 .list-group {
-  /*min-height: 20px;*/
+    /*min-height: 20px;*/
 }
 .list-group-item {
-  /*cursor: move;*/
+    /*cursor: move;*/
 }
 .list-group-item i {
-  /*cursor: pointer;*/
+    /*cursor: pointer;*/
 }
+
+.fade-enter,.fade-leave-to {
+    visibility: hidden;
+    height: 0;
+    margin: 0;
+    padding: 0;
+    opacity: 0;
+}
+
+.fade-enter-active,.fade-leave-active {
+    transition: all 0.3s;
+}
+
+
 </style>
+
+
+<style scoped src="@/assets/styles/main.css"></style>
