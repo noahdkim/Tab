cf400cf6695d4fdc5d7b3180fbc81dc6e04e5889
johnjp15 <johnjp15@gmail.com>
2019-04-18 14:38:36 -0400

    REBASE more-styling onto calendar-dev.

--- tab-app/src/components/Home/List/ListRow.vue
+++ tab-app/src/components/Home/List/ListRow.vue
@@ -38,37 +38,37 @@
         /* this is currently not being used */
         checkbox: true,
         showHandle: false,
-      }
-    },
-    methods: {
-        deleteItem (event) {
-            this.$store.dispatch('deleteItem', this.item);
-        },
-        makeActive (event) {
+    }
+},
+methods: {
+  deleteItem (event) {
+    this.$store.dispatch('deleteItem', this.item);
+},
+makeActive (event) {
               // When the row is clicked, dispatch changeActiveItem to the store passing
               // the current ID.
               // the changeActiveItem method searches for the ID and modifies the active attribute
               // to true
               this.showHandle = false;
               this.$store.dispatch('changeActiveItem', this.item);
-        },
-        mouseOver(event)    {
-            this.showHandle = true;
-            var vm = this;
+          },
+          mouseOver(event)    {
+              this.showHandle = true;
+              var vm = this;
 
-            EventBus.$on('the-list-drag-event', drag => {
+              EventBus.$on('the-list-drag-event', drag => {
                 // is this necessary? it seems to slow down code a good amount
                 if(drag)    {
-                    this.showHandle = false;
-                }
-            });
-        },
-        mouseLeave(event)   {
-            this.showHandle = false;
-        }
+                  this.showHandle = false;
+              }
+          });
+          },
+          mouseLeave(event)   {
+              this.showHandle = false;
+          }
 
-}
-}
+      }
+  }
 
 
 
@@ -76,39 +76,56 @@
 
 <style scoped>
 .button {
-  /*margin-top: 35px;*/
+    /*margin-top: 35px;*/
 }
 .handle {
-  padding: 5px;
-  margin-left: 10px;
-  margin-right: 10px;
-  /*border: solid #000 1px;*/
-  cursor: grab;
-  font-size: 200%;
-  /*margin-bottom: 30px;*/
+    padding: 5px;
+    margin-left: 10px;
+    margin-right: 10px;
+    /*border: solid #000 1px;*/
+    cursor: grab;
+    font-size: 200%;
+    /*margin-bottom: 30px;*/
 
-  color: rgba(0, 0, 0, 0.3);
+    color: rgba(0, 0, 0, 0.3);
 }
 .close {
-  float: right;
-  /*padding-top: 8px;*/
-  /*padding-bottom: 8px;*/
+    float: right;
+    /*padding-top: 8px;*/
+    /*padding-bottom: 8px;*/
 }
 input {
-  display: inline-block;
-  /*width: 50%;*/
+    display: inline-block;
+    /*width: 50%;*/
 }
 .text {
-  margin: 0px;
+    margin: 0px;
 }
 
 .list-group {
-  /*min-height: 20px;*/
+    /*min-height: 20px;*/
 }
 .list-group-item {
-  /*cursor: move;*/
+    /*cursor: move;*/
 }
 .list-group-item i {
-  /*cursor: pointer;*/
+    /*cursor: pointer;*/
 }
+
+.fade-enter,.fade-leave-to {
+    visibility: hidden;
+    height: 0;
+    margin: 0;
+    padding: 0;
+    opacity: 0;
+}
+
+.fade-enter-active,.fade-leave-active {
+    transition: all 0.3s;
+}
+
+
 </style>
+
+
+<style scoped src="@/assets/styles/main.css"></style>
