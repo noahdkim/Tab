a12d4ec53349bb64d1916540d3e8bfd84594a406
noahdkim <nk8na@virginia.edu>
2019-12-01 23:49:39 -0500

    edit about page

--- tab-app/src/components/AboutUs.vue
+++ tab-app/src/components/AboutUs.vue
@@ -1,8 +1,11 @@
 <template>
   <v-container fluid>
+    <v-col align-self="center" class="body-1" sm="6" justify-self="center" offset="3">
     Tab was created to help people flexibly organize their lives. We know no two people are the same
-    so why should their productivity managers?
+    so why should their productivity managers?<br> <br>
+
     Tab provides customization and organization in simple, powerful to-do lists
+    </v-col>
   </v-container>
 </template>
 
